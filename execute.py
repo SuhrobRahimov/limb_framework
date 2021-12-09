b49f88de0c43604ba9b2344391036e5450d8116d
FreedomToh3 <freedom.toh3@gmail.com>
2021-12-09 15:12:49 +0700

    Changed for uwsgi

--- execute.py
+++ execute.py
@@ -4,8 +4,9 @@ from wsgiref.simple_server import make_server
 import settings
 from limb_framework.core import Core
 
-app = Core(settings)
+application = Core(settings)
 
-with make_server(settings.HOST, settings.PORT, app) as httpd:
-    print(f"Запуск на порту {settings.PORT}...")
-    httpd.serve_forever()
+if __name__ == "__main__":
+    with make_server(settings.HOST, settings.PORT, application) as httpd:
+        print(f"Запуск на порту {settings.PORT}...")
+        httpd.serve_forever()
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Для запуска через uwsgi использовать команду
+	uwsgi --http :8000 --wsgi-file ./execute.py
